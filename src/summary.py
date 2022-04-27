09849eecf7c431f1956818cfc487a7379ea7f14c
Tobias Huber <Tobiashuber94@gmail.com>
2022-04-27 20:19:44 +0200

    Generating CFs for ACER HIGHLIGHTS

--- src/summary.py
+++ src/summary.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 from tensorflow import keras
+import torch
 
 from src.dataset_generation import _preprocess
 from src.evaluation import Evaluator
@@ -13,6 +14,7 @@ from src.util import init_environment, get_agent_prediction_from_stacked_frames,
     restrict_tf_memory, generate_olson_counterfactual, load_baselines_model
 
 from baselines.common.tf_util import adjust_shape
+from src.star_gan.model import Generator
 
 def generate_highlights_div_summary(env_name, agent, num_frames, num_simulations, interval_size, save_dir,
                                     power_pill_objective=False, max_noop=1, agent_type = "keras"):
@@ -214,7 +216,6 @@ if __name__ == "__main__":
 
     # Settings
     summary_dir = "../res/HIGHLIGHTS_DIV/Summaries/PacMan_FearGhost"
-    cf_summary_dir = "../res/HIGHLIGHTS_DIV/CF_Summaries/PacMan_FearGhost"
     env_name = "MsPacmanNoFrameskip-v4"
     # agent = keras.models.load_model("../res/agents/Pacman_Ingame_cropped_5actions_5M.h5")
     agent = load_baselines_model(r"../res/agents/ACER_PacMan_FearGhost_cropped_5actions_40M", num_actions=5, num_env=1)
@@ -223,6 +224,17 @@ if __name__ == "__main__":
     interval_size = 50
     num_simulations = 3
 
-    # Generate a summary that is saved in cf_summary_dir
+    nb_actions = 5
+    img_size = 176
+    # Load a StarGAN generator
+    generator = Generator(c_dim=nb_actions, channels=3).cuda()
+    generator.load_state_dict(torch.load("../res/models/PacMan_FearGhost_A_1/models/200000-G.ckpt",
+                                         map_location=lambda storage, loc: storage))
+    cf_summary_dir = "../res/HIGHLIGHTS_DIV/CF_Summaries/PacMan_FearGhost_A_1"
+
+    # Generate a summary that is saved in summary_dir
     generate_highlights_div_summary(env_name, agent, num_frames, num_simulations, interval_size, summary_dir,
                                     agent_type="acer")
+
+    # Generate CFs for that summary which are saved in cf_summary_dir
+    generate_summary_counterfactuals(summary_dir, generator, nb_actions, img_size, cf_summary_dir)
